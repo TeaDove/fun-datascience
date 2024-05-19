1b6f96bfe48dd66d29a25c0297b2a032c298ea40
pibragimov <pibragimov@whoosh.bike>
2024-05-19 21:50:56 +0300

    auto: autocommit

--- ds/presentation/anime_router.py
+++ ds/presentation/anime_router.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, HTTPException, UploadFile
 from presentation.dependencies import container
 from schemas.anime import AnimePredictionResponse
+from service.anime_service import NotAnImage
 
 router = APIRouter(prefix="/anime")
 
 
 @router.post("/predict", response_model=AnimePredictionResponse)
 async def amine_predict(image: UploadFile) -> AnimePredictionResponse:
-    return AnimePredictionResponse(
-        prediction=container.anime_service.make_prediction(await image.read())
-    )
+    try:
+        return AnimePredictionResponse(
+            prediction=container.anime_service.make_prediction(await image.read())
+        )
+    except NotAnImage:
+        raise HTTPException(status_code=422, detail="inputed file is not an image")
